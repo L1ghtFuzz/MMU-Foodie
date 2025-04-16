34c77a2b276cc248a0689811b820fd6babd02485
Aiden <airellaidan3@gmail.com>
2025-04-16 12:19:14 +0800

    The randomizer for restaurants
    still tryna figure out how to add new restaurants without opening the code

---
+++ Restaurants randomizer.py
@@ -0,0 +1,5 @@
+import random
+
+restaurants = ["Namo Garden", "Giggles & Geeks", "Woodfire Cyberjaya", "Lengis Restaurant Cyberjaya", "Kocha Lala", "After Seven Lounge", "10gram Cyberjaya", "Strawberry Fields Cafe Cyberjaya"]
+
+print(random.choice(restaurants))
