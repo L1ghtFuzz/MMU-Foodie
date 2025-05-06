c6482ce8709afcb318ea91f329c2ba073fa96189
Aiden <airellaidan3@gmail.com>
2025-05-06 23:34:04 +0800

    "Coding so far"

---
+++ Website/models.py
@@ -0,0 +1,31 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+# Association table for many-to-many relationship
+favourites = db.Table(
+    'favourites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurant.id'))
+)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    favourites = db.relationship('Restaurant', secondary='favourites', backref='liked_by')
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    cuisine = db.Column(db.String(100))
+    location = db.Column(db.String(150))
+    description = db.Column(db.String(255))
